cdf83823590a3f4c5321dea5d54a77a58a562d42
Jonathan Nicholas <jni68@uclive.ac.nz>
2023-05-26 16:47:35 +1200

    demand values printing as expected

--- network_flow.py
+++ network_flow.py
@@ -9,13 +9,25 @@ Z = 7
 #For appendix (X=3, Y=2, Z=3)
 
 if len(sys.argv) > 1:
-    X = sys.argv[1]
-    Y = sys.argv[2]
-    Z = sys.argv[3]
+    X = int(sys.argv[1])
+    Y = int(sys.argv[2])
+    Z = int(sys.argv[3])
 
 
 print("X =", X)
 print("Y =", Y)
 print("Z =", Z)
 
+print("Minimize\n")
+
+print("Subject to\n")
+
+# Demand value of paths equal to sum of ij as specified on page 3
+for x in range(1,X+1):
+    for z in range(1,Z+1):
+        for y in range(1,Y+1):
+            if y != Y:
+                print("x{}{}{} + ".format(x,y,z), end="")
+            else:
+                print("x{}{}{} = {}".format(x,y,z,x+z))
 
